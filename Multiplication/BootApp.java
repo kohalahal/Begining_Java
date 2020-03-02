772af4cd5436709177083ba26bd9beebf9520f65
Kohalahal <52988648+kohalahal@users.noreply.github.com>
2020-03-02 12:54:18 +0900

    Create BootApp.java

---
+++ Multiplication/BootApp.java
@@ -0,0 +1,21 @@
+public class BootApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		Gugudan gugu = new Gugudan();
+//		gugu.print();
+//		
+//		Gugudan2 gugu2 = new Gugudan2();
+//		gugu2.print();
+//		
+		Gugudan3 gugu3 = new Gugudan3();
+		gugu3.print();
+//		
+//		Gugudan4 gugu4 = new Gugudan4();
+//		gugu4.print();
+		
+		
+		
+	}
+
+}
